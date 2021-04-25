7884fa8cd6799d16276fa37b5ad86e2ba92c9813
Optimizer-Prime <stu3cla@protonmail.com>
2021-04-25 12:28:12 -0400

    First FT written

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,37 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # The user goes to the homepage for our site
+        self.browser.get('http://localhost:8000')
+
+        # The user notices that the page title mentions to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # User can enter item into list right away
+
+        # User types "Purchase laundry detergent" into text box
+
+        # User hits enter, page updates, page shows entered task
+
+        # Text box present for another entry
+
+        # page udpates again, shows both items
+
+        # website creates unqiue URL to remember her list
+
+        # user visits URL and list is still there
+
+
+if __name__ == '__main__':
+    unittest.main()
